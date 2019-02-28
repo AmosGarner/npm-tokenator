6289abd314d1bf66ae006d2f7cc4e5aa3764be93
Adam Williams <8483003+awilliams219@users.noreply.github.com>
2019-02-28 15:05:47 -0500

    Create module

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+
+let fetchToken = async (tokenSource) => {
+    return await fetch(tokenSource);
+};
+
+let Get = async (endpoint, tokenSource) => {
+    let token;
+
+    const forms = Array.prototype.slice.call(document.querySelectorAll(`form[action=${endpoint}]`));
+    if (forms.length < 1) {
+        console.warn(`RSC.Tokenator: No forms with action '${endpoint}'`);
+    }
+
+    try {
+        token = await fetchToken(tokenSource);
+    } catch {
+        console.error("Token Service is not responding or an error occurred.");
+        return;
+    }
+
+    for (let form in forms) {
+        let inputElement = document.createElement('input');
+        inputElement.setAttribute('type', 'hidden');
+        inputElement.setAttribute('name', 'token');
+        inputElement.setAttribute('value', token);
+
+        forms[form].appendChild(inputElement);
+    }
+};
+
+module.exports = {
+    Get
+};
